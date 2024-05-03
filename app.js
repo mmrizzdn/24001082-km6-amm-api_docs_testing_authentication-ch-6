06bc0fcedfa8b121700129adb478b16494313aed
mmrizzdn <zoed.eldien15@gmail.com>
2024-05-03 23:49:54 +0700

    init

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/v1/index');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', indexRouter);
+
+// 500 error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        status: false,
+        message: err.message,
+        data: null
+    });
+});
+
+// 404 error handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: false,
+        message: `are you lost? ${req.method} ${req.url} is not registered!`,
+        data: null
+    });
+});
 
 module.exports = app;
